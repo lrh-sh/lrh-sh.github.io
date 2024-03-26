323a63543dbcf697231b039240c1d36f674809d5
robertaue <42839069+robertaue@users.noreply.github.com>
2024-03-26 14:24:08 +0100

    debug statement

--- Schuldenquotenrechner/App.js
+++ Schuldenquotenrechner/App.js
@@ -3,6 +3,7 @@ import testResult from './MonteCarloSim.js';
 function App() {
     var out = document.getElementById('test_output');
     var res = testResult();
+    console.log("Hello from App()!")
     out.value = res.lrss;
     return 0;
 }
