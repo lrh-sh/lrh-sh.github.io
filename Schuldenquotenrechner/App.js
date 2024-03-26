afa85f639e347ce37b6de58333358a10e20877e4
robertaue <42839069+robertaue@users.noreply.github.com>
2024-03-26 15:03:23 +0100

    getting closer

--- Schuldenquotenrechner/App.js
+++ Schuldenquotenrechner/App.js
@@ -1,13 +1,15 @@
 import testResult from './MonteCarloSim.js';
 
+console.log("Hello from App.js!")
+
 function App() {
+    console.log("Hello from App()!")
     var out = document.getElementById('test_output');
     var res = testResult();
-    console.log("Hello from App()!")
-    out.value = res.lrss;
+    out.innerHTML = `Long run steady state: ${res.lrss}<br>Schuldenquote: ${res.oneMSsim.Schuldenquote}<br>Zins-Steuer-Quote: ${res.oneMSsim.ZinsSteuerQuote}`;
     return 0;
 }
 
-//App();
+App();
 
 export default App;
